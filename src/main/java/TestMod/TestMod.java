10602c1983ea013b8a3105d7d531bf2bee40bbff
Zim <98758788+hazimbharis@users.noreply.github.com>
2024-03-23 11:05:22 +0000

    Initial commit

---
+++ src/main/java/TestMod/TestMod.java
@@ -0,0 +1,49 @@
+package TestMod;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import basemod.BaseMod;
+import basemod.interfaces.PostBattleSubscriber;
+import basemod.interfaces.PostDungeonInitializeSubscriber;
+import basemod.interfaces.PostExhaustSubscriber;
+
+@SpireInitializer
+public class TestMod implements PostExhaustSubscriber,
+        PostBattleSubscriber, PostDungeonInitializeSubscriber {
+
+    private int count, totalCount;
+
+    private void resetCounts() {
+        totalCount = count = 0;
+    }
+
+    public TestMod() {
+        BaseMod.subscribe(this);
+        resetCounts();
+    }
+
+    public static void initialize() {
+        new TestMod();
+    }
+
+    @Override
+    public void receivePostExhaust(AbstractCard c) {
+        count++;
+        totalCount++;
+    }
+
+    @Override
+    public void receivePostBattle(AbstractRoom r) {
+        System.out.println(count + " cards were exhausted this battle, " +
+                totalCount + " cards have been exhausted so far this act.");
+        count = 0;
+    }
+
+    @Override
+    public void receivePostDungeonInitialize() {
+        resetCounts();
+    }
+
+}
