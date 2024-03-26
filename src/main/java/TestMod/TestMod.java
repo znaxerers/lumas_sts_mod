42082e40d8142e7884edc7ce67c499e298bb9389
Zim <104741739+znaxerers@users.noreply.github.com>
2024-03-26 13:17:28 +0000

    makes 1 placeholder relic
    we all gotta start somewhere

--- src/main/java/TestMod/TestMod.java
+++ src/main/java/TestMod/TestMod.java
@@ -1,19 +1,51 @@
 package TestMod;
 
+import basemod.helpers.RelicType;
+import basemod.interfaces.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.Gson;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.helpers.CardHelper;
+import com.megacrit.cardcrawl.localization.RelicStrings;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
-import basemod.BaseMod;
-import basemod.interfaces.PostBattleSubscriber;
-import basemod.interfaces.PostDungeonInitializeSubscriber;
-import basemod.interfaces.PostExhaustSubscriber;
+import basemod.*;
+//import basemod.interfaces.EditRelicsSubscriber;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import util.IDCheckDontTouchPls;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 @SpireInitializer
-public class TestMod implements PostExhaustSubscriber,
+public class TestMod implements EditRelicsSubscriber, EditStringsSubscriber, PostExhaustSubscriber,
         PostBattleSubscriber, PostDungeonInitializeSubscriber {
 
     private int count, totalCount;
+    public static final Logger logger = LogManager.getLogger(TestMod.class.getName());
+    private static String modID;
+
+    // Mod-settings settings. This is if you want an on/off savable button
+    public static Properties theDefaultDefaultSettings = new Properties();
+    public static final String ENABLE_PLACEHOLDER_SETTINGS = "enablePlaceholder";
+    public static boolean enablePlaceholder = true; // The boolean we'll be setting on/off (true/false)
+
+    //This is for the in-game mod settings panel.
+    private static final String MODNAME = "Luma Mod";
+    private static final String AUTHOR = "Zim"; // And pretty soon - You!
+    private static final String DESCRIPTION = "A base for Slay the Spire to start your own mod from, feat. the Default.";
+
+    // =============== INPUT TEXTURE LOCATION =================
+    // Colors (RGB)
+    // Character Color
+    public static final Color DEFAULT_GRAY = CardHelper.getColor(64.0f, 70.0f, 70.0f);
 
     private void resetCounts() {
         totalCount = count = 0;
@@ -21,10 +53,13 @@ public class TestMod implements PostExhaustSubscriber,
 
     public TestMod() {
         BaseMod.subscribe(this);
+        setModID("TestMod");
+
         resetCounts();
     }
 
     public static void initialize() {
+        System.out.println(">>>Initializing gooning mode");
         new TestMod();
     }
 
@@ -44,6 +79,88 @@ public class TestMod implements PostExhaustSubscriber,
     @Override
     public void receivePostDungeonInitialize() {
         resetCounts();
+    }
+
+    @Override
+    public void receiveEditStrings() {
+        logger.info("Testing strings");
+        BaseMod.loadCustomStringsFile(RelicStrings.class, getModID() + "Resources/localization/eng/TestMod-Relic-Strings.json");
+        // Loads in strings that contain your relic name, descriptions and flavor.
+
+        logger.info("Done testing strings");
+    }
+
+    @Override
+    public void receiveEditRelics() {
+        logger.info("Adding relics");
+
+        // Take a look at https://github.com/daviscook477/BaseMod/wiki/AutoAdd
+        // as well as
+        // https://github.com/kiooeht/Bard/blob/e023c4089cc347c60331c78c6415f489d19b6eb9/src/main/java/com/evacipated/cardcrawl/mod/bard/BardMod.java#L319
+        // for reference as to how to turn this into an "Auto-Add" rather than having to list every relic individually.
+        // Of note is that the bard mod uses it's own custom relic class (not dissimilar to our AbstractDefaultCard class for cards) that adds the 'color' field,
+        // in order to automatically differentiate which pool to add the relic too.
+
+        // This adds a character specific relic. Only when you play with the mentioned color, will you get this relic.
+        //BaseMod.addRelicToCustomPool(new TestRelic(), AbstractCard.CardColor.COLORLESS);
+
+        // This adds a relic to the Shared pool. Every character can find this relic.
+        BaseMod.addRelic(new TestRelic(), RelicType.SHARED);
+
+        // Mark relics as seen - makes it visible in the compendium immediately
+        // If you don't have this it won't be visible in the compendium until you see them in game
+        // (the others are all starters so they're marked as seen in the character file)
+        UnlockTracker.markRelicAsSeen(TestRelic.ID);
+        logger.info("Done adding relics!");
+    }
+
+    public static void setModID(String ID) { // DON'T EDIT
+        Gson coolG = new Gson(); // EY DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf(StandardCharsets.UTF_8)); // i hate u Gdx.files
+        InputStream in = TestMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT THIS ETHER
+        IDCheckDontTouchPls EXCEPTION_STRINGS = coolG.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), IDCheckDontTouchPls.class); // OR THIS, DON'T EDIT IT
+        logger.info("You are attempting to set your mod ID as: " + ID); // NO WHY
+        if (ID.equals(EXCEPTION_STRINGS.DEFAULTID)) { // DO *NOT* CHANGE THIS ESPECIALLY, TO EDIT YOUR MOD ID, SCROLL UP JUST A LITTLE, IT'S JUST ABOVE
+            throw new RuntimeException(EXCEPTION_STRINGS.EXCEPTION); // THIS ALSO DON'T EDIT
+        } else if (ID.equals(EXCEPTION_STRINGS.DEVID)) { // NO
+            modID = EXCEPTION_STRINGS.DEFAULTID; // DON'T
+        } else { // NO EDIT AREA
+            modID = ID; // DON'T WRITE OR CHANGE THINGS HERE NOT EVEN A LITTLE
+        } // NO
+        logger.info("Success! ID is " + modID); // WHY WOULD U WANT IT NOT TO LOG?? DON'T EDIT THIS.
+    } // NO
+
+    private static void pathCheck() { // ALSO NO
+        Gson coolG = new Gson(); // NOPE DON'T EDIT THIS
+        //   String IDjson = Gdx.files.internal("IDCheckStringsDONT-EDIT-AT-ALL.json").readString(String.valueOf(StandardCharsets.UTF_8)); // i still hate u btw Gdx.files
+        InputStream in = TestMod.class.getResourceAsStream("/IDCheckStringsDONT-EDIT-AT-ALL.json"); // DON'T EDIT THISSSSS
+        IDCheckDontTouchPls EXCEPTION_STRINGS = coolG.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), IDCheckDontTouchPls.class); // NAH, NO EDIT
+        String packageName = TestMod.class.getPackage().getName(); // STILL NO EDIT ZONE
+        FileHandle resourcePathExists = Gdx.files.internal(getModID() + "Resources"); // PLEASE DON'T EDIT THINGS HERE, THANKS
+        if (!modID.equals(EXCEPTION_STRINGS.DEVID)) { // LEAVE THIS EDIT-LESS
+            if (!packageName.equals(getModID())) { // NOT HERE ETHER
+                throw new RuntimeException(EXCEPTION_STRINGS.PACKAGE_EXCEPTION + getModID()); // THIS IS A NO-NO
+            } // WHY WOULD U EDIT THIS
+            if (!resourcePathExists.exists()) { // DON'T CHANGE THIS
+                throw new RuntimeException(EXCEPTION_STRINGS.RESOURCE_FOLDER_EXCEPTION + getModID() + "Resources"); // NOT THIS
+            }// NO
+        }// NO
+    }// NO
+
+    public static String getModID() { // NO
+        return modID; // DOUBLE NO
+    } // NU-UH
+
+    public static String makeRelicPath(String resourcePath) {
+        return getModID() + "Resources/images/relics/" + resourcePath;
+    }
+
+    public static String makeRelicOutlinePath(String resourcePath) {
+        return getModID() + "Resources/images/relics/outline/" + resourcePath;
+    }
+
+    public static String makeID(String idText) {
+        return getModID() + ":" + idText;
     }
 
 }
